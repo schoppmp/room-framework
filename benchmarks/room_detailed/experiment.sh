34e432199e828d128a11538ab38da4ac157baa72
Phillipp Schoppmann <schoppmann@informatik.hu-berlin.de>
2018-07-09 18:06:34 +0200

    Add script for detailed ROOM experiments

---
+++ benchmarks/room_detailed/experiment.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+config=$(mktemp)
+for n in 500 5000 50000; do
+  for m_frac in {1..10}; do
+    m=$(((n * m_frac) / 10))
+    for type in poly scs fss_cprg; do
+      echo "num_elements_server = $n" >> $config
+      echo "num_elements_client = $m" >> $config
+      echo "pir_type= $type" >> $config
+    done
+  done
+done
+cat >> $config << EOF
+statistical_security = 40
+
+[server]
+host=localhost
+port=12347
+
+[server]
+host=localhost
+port=12357
+EOF
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+$DIR/../../bin/benchmark/pir -c $config "$@"
