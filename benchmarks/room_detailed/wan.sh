67827e50696cf1bdc3159dca2d6cc0da97e930df
Phillipp Schoppmann <schoppmann@informatik.hu-berlin.de>
2018-07-16 19:09:28 +0200

    Add WAN experiment script for ROOM microbenchmarks
    same as LAN, but without FSS

---
+++ benchmarks/room_detailed/wan.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+config=$(mktemp)
+for n in 500 5000 50000; do
+  for m_frac in {1..10}; do
+    m=$(((n * m_frac) / 10))
+    for type in poly scs; do
+      echo "num_elements_server = $n" >> $config
+      echo "num_elements_client = $m" >> $config
+      echo "pir_type= $type" >> $config
+    done
+  done
+done
+cat >> $config << EOF
+statistical_security = 40
+EOF
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+$DIR/../../bin/benchmark/pir -c $config "$@"
