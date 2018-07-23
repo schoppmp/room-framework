5c30a554c1d5ccd582d312f4880b78941020dd8b
Phillipp Schoppmann <schoppmann@informatik.hu-berlin.de>
2018-07-23 14:59:07 -0400

    Add zero_sharing test binary
    It compiles and runs without leaks; obliv-c part still missing

--- src/zero_sharing/zero_sharing.hpp
+++ src/zero_sharing/zero_sharing.hpp
@@ -1,10 +1,19 @@
 #pragma once
 extern "C" {
   #include "obliv_common.h"
+  #include "zero_sharing.h"
 }
+#include "mpc-utils/comm_channel.hpp"
+#include "util/serialize_le.hpp"
+#include "fastpoly/recursive.h"
 #include <Eigen/Dense>
 #include <random>
 #include <gcrypt.h>
+#include <boost/exception/all.hpp>
+#include <boost/serialization/vector.hpp>
+#include <NTL/vector.h>
+#include <NTL/ZZ.h>
+#include <NTL/ZZ_pX.h>
 
 // Inputs:
 // Server - A share of a short vector v of length l
@@ -19,13 +28,14 @@ template<typename T>
 std::vector<T> zero_sharing_server(
   std::vector<T> v,
   std::vector<size_t> I,
-  size_t n
+  size_t n,
+  comm_channel& chan
 ) {
   size_t l = v.size();
   if(I.size() != v.size()) {
     BOOST_THROW_EXCEPTION(std::runtime_error("v and I need to have the same length"));
   }
-  NTL::ZZ modulus(modulus((NTL::ZZ(1) << 128) - 159));
+  NTL::ZZ modulus((NTL::ZZ(1) << 128) - 159);
   NTL::ZZ_pPush push(modulus);
   gcryDefaultLibInit();
   auto cipher = GCRY_CIPHER_AES128;
@@ -34,7 +44,7 @@ std::vector<T> zero_sharing_server(
   const size_t block_size = 16;
   const size_t element_size = sizeof(T) + block_size;
   // set up OT arguments
-  bool *choices = calloc(n, sizeof(bool));
+  bool *choices = (bool *) calloc(n, sizeof(bool));
   for(auto i : I) {
     choices[i] = 1;
   }
@@ -46,20 +56,24 @@ std::vector<T> zero_sharing_server(
   }
   setCurrentParty(&pd, 1);
   auto ot = honestOTExtRecverNew(&pd, 0);
-  honestOTExtRecv1Of2(ot, ot_result.data(), choices, n, element_size);
+  honestOTExtRecv1Of2(ot, reinterpret_cast<char *>(ot_result.data()), choices,
+    n, element_size);
   honestOTExtRecverRelease(ot);
   // unpack
   std::vector<T> s(n);
   std::vector<T> t(l); // encrypted shares corresponding to indices in I
-  NTL::Vec<NTL::ZZ_p> share_K(l);
-  NTL::Vec<NTL::ZZ_p> interpolate_pos(l);
+  NTL::Vec<NTL::ZZ_p> share_K;
+  NTL::Vec<NTL::ZZ_p> interpolate_pos;
+  share_K.SetLength(l);
+  interpolate_pos.SetLength(l);
   size_t num_shares = 0;
   for(size_t i = 0; i < n; i++) {
     if(choices[i]) {
       deserialize_le(&t[num_shares], &ot_result[i * element_size], 1);
-      NTL::conv(&share_K[num_shares], NTL::ZZFromBytes(
+      NTL::conv(share_K[num_shares], NTL::ZZFromBytes(
         &ot_result[i * element_size + sizeof(T)], block_size));
-      NTL::conv(&interpolate_pos[num_shares], i+1);
+      NTL::conv(interpolate_pos[num_shares], i+1);
+      num_shares++;
     } else {
       deserialize_le(&s[i], &ot_result[i * element_size], 1);
     }
@@ -68,19 +82,21 @@ std::vector<T> zero_sharing_server(
   NTL::ZZ_pX poly;
   poly_interpolate_zp_recursive(l - 1, interpolate_pos.data(), share_K.data(),
     poly);
+  // std::cout << "interpolate_pos = " << interpolate_pos << "\n";
+  // std::cout << "share_K = " << share_K << "\n";
+  // std::cout << "poly = " << poly << "\n";
   std::vector<uint8_t> K(block_size);
   NTL::BytesFromZZ(K.data(), NTL::conv<NTL::ZZ>(NTL::ConstTerm(poly)), block_size);
   // decrypt shares not in I
   gcry_cipher_open(&handle, cipher, GCRY_CIPHER_MODE_CTR, 0);
   gcry_cipher_setkey(handle, K.data(), block_size);
-  std::vector<T> s(n);
   for(size_t i = 0; i < n; i++) {
     if(choices[i]) {
       continue;
     }
     uint8_t buf[block_size] = {0};
     uint8_t ctr[block_size] = {0};
-    serialize_le(&ctr, i, 1);
+    serialize_le(&ctr[0], &i, 1);
     gcry_cipher_setctr(handle, ctr, block_size);
     gcry_cipher_encrypt(handle, buf, block_size, nullptr, 0);
     T r;
@@ -90,8 +106,13 @@ std::vector<T> zero_sharing_server(
   gcry_cipher_close(handle);
 
   // TODO: decrypt shares in yao protocol
+  zero_sharing_oblivc_args args;
+  execYaoProtocol(&pd, zero_sharing_oblivc, &args);
+  auto& ret = s;
+
   free(choices);
   cleanupProtocol(&pd);
+  return ret;
 }
 
 template<typename T>
@@ -120,7 +141,7 @@ std::vector<T> zero_sharing_client(
   for(size_t i = 0; i < n; i++) {
     uint8_t buf[block_size] = {0};
     uint8_t ctr[block_size] = {0};
-    serialize_le(&ctr, i, 1);
+    serialize_le(&ctr[0], &i, 1);
     gcry_cipher_setctr(handle, ctr, block_size);
     gcry_cipher_encrypt(handle, buf, block_size, nullptr, 0);
     deserialize_le(&s[i], buf, 1);
@@ -132,7 +153,7 @@ std::vector<T> zero_sharing_client(
   for(size_t i = 0; i < n; i++) {
     uint8_t buf[block_size] = {0};
     uint8_t ctr[block_size] = {0};
-    serialize_le(&ctr, i, 1);
+    serialize_le(&ctr[0], &i, 1);
     gcry_cipher_setctr(handle, ctr, block_size);
     gcry_cipher_encrypt(handle, buf, block_size, nullptr, 0);
     deserialize_le(&t[i], buf, 1);
@@ -140,10 +161,12 @@ std::vector<T> zero_sharing_client(
   }
   gcry_cipher_close(handle);
   // secret-share K
-  NTL::ZZ modulus(modulus((NTL::ZZ(1) << 128) - 159));
+  NTL::ZZ modulus((NTL::ZZ(1) << 128) - 159);
   NTL::ZZ_pPush push(modulus);
-  NTL::Vec<NTL::ZZ_p> share_K(n);
-  NTL::Vec<NTL::ZZ_p> eval_pos(n); // where the polynomial will be evaluated
+  NTL::Vec<NTL::ZZ_p> share_K;
+  NTL::Vec<NTL::ZZ_p> eval_pos; // where the polynomial will be evaluated
+  share_K.SetLength(n);
+  eval_pos.SetLength(n);
   std::iota(eval_pos.begin(), eval_pos.end(), 1);
   auto poly = NTL::random_ZZ_pX(l); // degree-(l-1) polynomial
   if(NTL::deg(poly) != l - 1) {
@@ -153,7 +176,10 @@ std::vector<T> zero_sharing_client(
   NTL::ZZ_p K_coeff;
   NTL::conv(K_coeff, NTL::ZZFromBytes(K.data(), block_size));
   NTL::SetCoeff(poly, 0, K_coeff);
-  poly_evaluate_zp_recursive(n - 1, poly, eval_pos, share_K);
+  poly_evaluate_zp_recursive(n - 1, poly, eval_pos.data(), share_K.data());
+  // std::cout << "eval_pos = " << eval_pos << "\n";
+  // std::cout << "share_K = " << share_K << "\n";
+  // std::cout << "poly = " << poly << "\n";
   // set up OT arguments (we are the sender)
   const size_t element_size = sizeof(T) + block_size; // one element of t + one share of K
   std::vector<uint8_t> opt0(element_size * n, 0);
@@ -172,9 +198,13 @@ std::vector<T> zero_sharing_client(
   }
   setCurrentParty(&pd, 2);
   auto ot = honestOTExtSenderNew(&pd, 0);
-  honestOTExtSend1Of2(ot, opt0, opt1, n, element_size);
+  honestOTExtSend1Of2(ot, reinterpret_cast<char*>(opt0.data()),
+    reinterpret_cast<char*>(opt1.data()), n, element_size);
   honestOTExtSenderRelease(ot);
   // TODO: run yao protocol to generate server's shares
+  zero_sharing_oblivc_args args;
+  execYaoProtocol(&pd, zero_sharing_oblivc, &args);
 
   cleanupProtocol(&pd);
+  return s;
 }
