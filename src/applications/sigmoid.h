adbcee1265b51677afcc968c10818f9e116f8c94
Phillipp Schoppmann <schoppmann@informatik.hu-berlin.de>
2018-11-12 08:20:47 +0100

    Add sigmoid.oc

---
+++ src/applications/sigmoid.h
@@ -0,0 +1,11 @@
+#pragma once
+
+typedef struct {
+  const int num_elements;
+  const int element_size;
+  const int precision;
+  const uint8_t *input;
+  uint8_t *output;
+} sigmoid_oblivc_args;
+
+void sigmoid_oblivc(void *);
